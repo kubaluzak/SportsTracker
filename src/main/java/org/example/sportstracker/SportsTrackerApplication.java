1c66cfa9f526da874f393628ef6105dd2bedb81c
kubaluzak <jakub.urbaniak88@gmail.com>
2026-04-23 15:30:57 +0200

    Initial commit

---
+++ src/main/java/org/example/sportstracker/SportsTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.sportstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sportstracker/SportsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sportstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
